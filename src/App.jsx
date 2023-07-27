a37ee27e43f51a12f5e189bd0f712847127ee333
Lizette Dimalanta <105244120+Lizette-Dimalanta@users.noreply.github.com>
2023-07-27 21:02:55 +1000

    Added bell, chat and plus icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { useState } from 'react'
 import './index.css'
 import Logo from './images/reddit-logo.png'
-import { BsSearch } from 'react-icons/bs'
+import { BsSearch, BsChatDots, BsBell } from 'react-icons/bs'
+import { AiOutlinePlus } from "react-icons/ai"
 
 function App() {
   const [count, setCount] = useState(0)
@@ -16,6 +17,9 @@ function App() {
         <BsSearch className="text-gray-500 h-5 w-5 pl-1 flex self-center"/>
         <input type="text" className="bg-reddit_dark-brighter h-6 text-sm pl-3 focus:outline-none text-white" placeholder="Search"/>
       </form>
+      <BsChatDots className="text-gray-300 w-6 h-6 m-1 mx-2" />
+      <BsBell className="text-gray-300 w-6 h-6 m-1 mx-2"/>
+      <AiOutlinePlus className="text-gray-300 w-8 h-8 mx-1" />
     </header>
     </>
   )
