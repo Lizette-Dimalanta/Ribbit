a078ad037fcc2a5075eea19b77e6b28c6d626208
Lizette Dimalanta <105244120+Lizette-Dimalanta@users.noreply.github.com>
2023-08-01 22:02:10 +1000

    Added new post form

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './index.css'
 import Header from './components/Header'
 import BoardHeader from './components/BoardHeader'
+import Avatar from './images/Avatar.png'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,6 +11,15 @@ function App() {
     <>
     <Header />
     <BoardHeader />
+
+    <div className="bg-reddit_dark px-6 py-4 text-gray-400">
+      <div className="border border-reddit_border p-2 rounded-md flex items-start">
+          <img src={Avatar} alt="" className="rounded-full overflow-hidden w-10 h-10" />
+        <form action="" className="w-full bg-reddit_dark-brighter border border-reddit_border ml-4 mr-2 rounded-md">
+          <input type="text" className="bg-reddit_dark-brighter p-2 px-3 text-sm w-full" placeholder="Create Post" />
+        </form>
+      </div>
+    </div>
     </>
   )
 }
