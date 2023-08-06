74ee8b8de01685534bf2f6883a8bca504c3a6b98
Lizette Dimalanta <105244120+Lizette-Dimalanta@users.noreply.github.com>
2023-08-06 20:54:54 +1000

    Added login toggle, added login/sign up buttons (toggled)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,11 +4,12 @@ import { BsSearch, BsChatDots, BsBell, BsChevronDown } from 'react-icons/bs'
 import { AiOutlinePlus } from "react-icons/ai"
 import Avatar from '../images/Avatar.png'
 import Button from './Button'
+import { CiUser, CiLogin } from "react-icons/ci"
 
 const Header = () => {
   return (
     <header className="w-full bg-reddit_dark p-2">
-        <div className="mx-4 flex">
+        <div className="mx-4 flex relative">
             <img src={Logo} alt="" className="w-[33px] h-[33px] mr-4 flex self-center"/>
             <form action="" className="bg-reddit_dark-brighter p-1 flex rounded-full border-[1px] border-reddit_border mx-4 flex-grow">
                 <BsSearch className="text-gray-500 h-5 w-5 pl-1 flex self-center"/>
@@ -29,12 +30,19 @@ const Header = () => {
                 <Button outline={false} className="">Sign Up</Button>
             </div>
         
-            <button className="rounded-md flex self-center ml-5">
-                <div className="w-8 h-8">
-                    <img src={Avatar} alt="" className="w-8 h-8 block rounded-md" />
-                </div>
-                <BsChevronDown className="text-gray-400 w-4 h-4 mt-2 ml-2"/>
+            <button className="rounded-md flex self-center ml-5 border border-gray-500">
+                <CiUser className="w-6 h-6 text-gray-400 m-1" />
+                {/* <div className="w-8 h-8"> */}
+                    {/* <img src={Avatar} alt="" className="w-8 h-8 block rounded-md" /> */}
+                {/* </div> */}
+                <BsChevronDown className="text-gray-400 w-4 h-4 mt-2 mr-2"/>
             </button>
+            <div className="absolute right-0 top-8 bg-reddit_dark border border-gray-700 z-10 rounded-md text-reddit_text">
+                <a href="" className="w-50 py-2 px-3 text-sm flex items-center hover:bg-gray-300 hover:text-black">
+                    <CiLogin className="w-5 h-5 mr-2" />
+                    Log In / Sign Up
+                </a>
+            </div>
         </div>
     </header>
   )
