096ef8a264ee04972a19a279322a3f1a31afa514
Lizette Dimalanta <105244120+Lizette-Dimalanta@users.noreply.github.com>
2023-08-13 18:15:01 +1000

    Completed register page, added express and mongoose

--- src/components/AuthModal.jsx
+++ src/components/AuthModal.jsx
@@ -1,9 +1,18 @@
 import { React, useState } from 'react'
 import Input from './Input'
 import Button from './Button'
+// import axios from 'axios'
 
 const AuthModal = () => {
   const [modalType, setModalType] = useState("login")
+  const [email, setEmail ] = useState("")
+  const [username, setUsername ] = useState("")
+  const [password, setPassword ] = useState("")
+
+  function register(){
+    e.preventDefault()
+    axios.post()
+  }
 
   return (
     <div className="w-screen h-screen fixed top-0 left-0 z-20 flex" style={{backgroundColor:'rgba(0,0,0,.6)'}}>
@@ -18,12 +27,22 @@ const AuthModal = () => {
             Register
           </h1>
         )}
+        {modalType === "register" && (
+          <div className="mb-3">
+          <label>
+            <span className="text-reddit_text-darker text-xs">
+              EMAIL
+            </span>
+            <Input type="email" className="mb-3 w-full" value={email} onChange={e => setEmail(e.target.value)} />
+          </label>
+        </div>
+        )}
         <div className="mb-3">
           <label>
             <span className="text-reddit_text-darker text-xs">
               USERNAME
             </span>
-            <Input type="text" className="mb-3 w-full" />
+            <Input type="text" className="mb-3 w-full" value={username} onChange={e => setUsername(e.target.value)} />
           </label>
         </div>
         <div className="mb-3">
@@ -31,18 +50,26 @@ const AuthModal = () => {
             <span className="text-reddit_text-darker text-xs">
               PASSWORD
             </span>
-            <Input type="password" className="mb-3 w-full" />
+            <Input type="password" className="mb-3 w-full" value={password} onChange={e => setPassword(e.target.value)} />
           </label>
         </div>
-        <Button className="w-full py-2" style={{borderRadius:'.3rem'}}>
-          Log In
-        </Button>
+        {modalType === "login" && (
+          <Button className="w-full py-2 mb-3" style={{borderRadius:'.3rem'}}>
+            Log In
+          </Button>
+        )}
+        {modalType === "register" && (
+          <Button className="w-full py-2 mb-3" style={{borderRadius:'.3rem'}} onClick={e => register(e)}>
+            Sign Up
+          </Button>
+        )}
         {modalType === "login" && (
           <div>
             New to Ribbit? 
             <button onClick={() => setModalType("register")} className="text-green-600 font-bold pl-2">
               SIGN UP
             </button>
+            
           </div>  
         )}
         {modalType === "register" && (
