caf7c18c3fb08d88c1a4aa31cb01371f0ec10266
Lizette Dimalanta <105244120+Lizette-Dimalanta@users.noreply.github.com>
2023-08-15 21:29:57 +1000

    Added databases and connection string

--- src/api/server.js
+++ src/api/server.js
@@ -1,11 +1,14 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import cookieParser from 'cookie-parser'
-import * as mongoose from 'mongoose'
+import mongoose from 'mongoose'
 import cors from 'cors'
 // import dotenv from 'dotenv'
 
 // dotenv.config()
+mongoose.connect('mongodb+srv://dbUser1:jVlPnrOeMRQBM9rd@cluster0.temziy2.mongodb.net/ribbit') // Connection string
+  .then((m) => console.log(m.connection.readyState === 1 ? 'Mongoose connected!' : 'Mongoose failed to connect')) // Check connection
+  .catch((err) => console.log(err))
 
 const app = express()
 app.use(bodyParser.urlencoded({ extended:true }))
@@ -14,6 +17,9 @@ app.use(cors({
     origin: 'localhost:3000',
     credentials: true
 }))
+
+const db = mongoose.connection
+db.on('error', console.log)
 
 app.get('/', (req, res) => {
     res.send('ok')
